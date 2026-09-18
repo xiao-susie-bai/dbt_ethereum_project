6477374a17f231a75232007475ea42db1e9f1822
susie bai <susiedream2019@gmail.com>
2026-09-18 00:36:20 -0400

    python_model (though Snowpark setup bug caused not running) 2026.09.18

--- models/marts/python_model.py
+++ models/marts/python_model.py
@@ -1,3 +1,28 @@
+
+import holidays
+
+def is_holiday(date_col): 
+    german_holidays = holidays.Germany()
+    is_holiday = (date_col in german_holidays)      #boolean
+    return is_holiday
+
 def model(dbt, session): 
+    dbt.config(packages=['pandas==2.3.3', 'holidays==0.29'])
+    import pandas as pd
+
+
     my_sql_model_df = dbt.ref("stablecoin_activity_per_day")
-    return my_sql_model_df
+    #my_sql_model_df = my_sql_model_df.to_pandas()
+    rows = my_sql_model_df.collect()
+    sql_model_df = pd.DataFrame([r.as_dict() for r in rows])
+
+    sql_model_df["is_holiday"] = sql_model_df["DATE"].apply(is_holiday)
+
+    snowpark_df = session.write_pandas(
+        sql_model_df, 
+        table_name='PYTHON_MODEL',
+        auto_create_table = True, 
+        overwrite = True, 
+        table_type = "temporary"
+    )
+    return snowpark_df
